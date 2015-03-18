883f182a72b5bc38c0289639de6e43e2ba6bc46c
Andre <Andre@Andre-PC>
2015-03-18 17:55:16 +0000

    Improvements to the main function of DebuggerCLI. It can now parse commands if they are on arg[1]

--- Assignment1/src/ist/meic/pa/DebuggerCLI.java
+++ Assignment1/src/ist/meic/pa/DebuggerCLI.java
@@ -1,16 +1,51 @@
 package ist.meic.pa;
 
+import ist.meic.pa.command.Command;
+import ist.meic.pa.command.parser.AbortCommandParser;
+import ist.meic.pa.command.parser.CommandParser;
+
+import java.util.ArrayList;
+
 public class DebuggerCLI {
-	static Object _runningClass = null;
-	static String _arguments[] = null;
+	static Object runningClass = null;
+	static String arguments[] = null;
+	private ArrayList<CommandParser> parsers;
+	
+	/**
+	 * Instantiates the array of parsers and adds ALL the parsers
+	 */
+	DebuggerCLI(){
+		parsers = new ArrayList<CommandParser>();
+		parsers.add(new AbortCommandParser(this));
+	}
+	
+	/**
+	 * Finds if there is a Command that can parse the line
+	 * Returns the Command if it exists and null if doesn't
+	 * 
+	 * @param line
+	 * @return parsedCommand null
+	 */
+	private Command parseCommand(String line) {
+		Command parsedCommand = null;
+		for (CommandParser parser : parsers) {
+			parsedCommand = parser.parseCommand(line);
+			if(parsedCommand != null)
+				break;
+		}
+		System.out.println(parsedCommand.getClass().getName());
+		return parsedCommand;
+	}
 	
 	public static void main(String[] args) throws Exception {
-		_runningClass = args[0];
+		
+		runningClass = args[0];
 		
 		for(int i=1; i<args.length; i++){
-			_arguments[i-1]=args[i];
+			arguments[i-1]=args[i];
 		}
-		
-		
+		DebuggerCLI debugger = new DebuggerCLI();
+		Command commmand = debugger.parseCommand(args[1]);
 	}
+
 }
