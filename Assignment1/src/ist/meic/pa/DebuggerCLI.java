03353c8032283078d8c6596e1abc2093a6c752bd
Andre <Andre@Andre-PC>
2015-03-19 21:57:00 +0000

    Added a new test class
    Debugger now prints the promp and checks if it is a command

--- Assignment1/src/ist/meic/pa/DebuggerCLI.java
+++ Assignment1/src/ist/meic/pa/DebuggerCLI.java
@@ -5,14 +5,31 @@ import ist.meic.pa.command.parser.AbortCommandParser;
 import ist.meic.pa.command.parser.CommandParser;
 import ist.meic.pa.test.TestException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Locale.Category;
 
 public class DebuggerCLI {
-	static Class<?> runningClass;
-	static String arguments[];
+	private Class<?> runningClass;
+	private String arguments[];
 	private ArrayList<CommandParser> parsers;
+	
+	/** Input stream reader for the Debugger.*/
+	private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+	/**
+	 *  True if an exception was caught when running the testing class
+	 */
+	private boolean catchedException = false;
+	
+	/**
+	 *  True if line received from user is a legal command
+	 */
+	private boolean commandFound = false;
 	
 	/**
 	 * Instantiates the array of parsers and adds ALL the parsers
@@ -40,26 +57,44 @@ public class DebuggerCLI {
 		return parsedCommand;
 	}
 	
-	public static void main(String[] args){
+	/**
+	 * Prints the prompt to receiver the user input after an exception was caught
+	 * 
+	 * @param messsage - message to be shown in the prompt
+	 * @return line - input from the user
+	 * @throws IOException
+	 */
+	private String promptUser(String message) throws IOException {
+		System.err.print(message);
+		String line = in.readLine();
+		return line;
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		DebuggerCLI debugger = new DebuggerCLI();
 		try{
-			runningClass = Class.forName("ist.meic.pa.test.TestClass");
+			debugger.setRunningClass( Class.forName("ist.meic.pa.test.TestClassDoesntThrowException"));
 		}catch(ClassNotFoundException e){
 			System.out.println("Could not find class: "+e.getMessage());
 		}
+		
 		for(int i=1; i<args.length; i++){
-			arguments[i-1]=args[i];
+			debugger.getArguments()[i-1]=args[i];
 		}
-		/*DebuggerCLI debugger = new DebuggerCLI();
-		Command commmand = debugger.parseCommand(args[1]);*/
+		
 		try { 
-			runningClass.getMethod("main", String[].class).invoke(runningClass, new String[]{ null });		
+			debugger.getRunningClass().getMethod("main", String[].class).invoke(debugger.getRunningClass(), new String[]{ null });
 		}
 		catch (InvocationTargetException e) { 
+			debugger.setCatchedException(true);
 			try { 
 				throw e.getCause(); 
 			}
 			catch (TestException te) { 
-				System.out.println("Ola");
+				/*
+				 * catches this exception
+				 */
 			}
 			catch(Throwable what){
 				System.out.println("What?");
@@ -72,6 +107,86 @@ public class DebuggerCLI {
 			System.out.println(e.getMessage());
 		}
 		
+		if(!debugger.isCatchedException()){
+			System.out.println("No exception was thrown. Class is ok");
+			System.exit(1);
+		}
+		
+		String inputLine = debugger.promptUser(">:");
+		Command com = debugger.parseCommand(inputLine);
+		if(com != null && com.canExecute()) {
+			debugger.setCommandFound(true);
+			com.execute();
+		}
+		if (!debugger.isCommandFound())
+			System.err.println("No command was executed.");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public Class<?> getRunningClass() {
+		return runningClass;
+	}
+
+	public void setRunningClass(Class<?> runningClass) {
+		this.runningClass = runningClass;
+	}
+
+	public String[] getArguments() {
+		return arguments;
+	}
+
+	public void setArguments(String[] arguments) {
+		this.arguments = arguments;
+	}
+
+	public ArrayList<CommandParser> getParsers() {
+		return parsers;
+	}
+
+	public void setParsers(ArrayList<CommandParser> parsers) {
+		this.parsers = parsers;
+	}
+
+	public BufferedReader get_in() {
+		return in;
+	}
+
+	public void set_in(BufferedReader _in) {
+		this.in = _in;
+	}
+
+	public boolean isCatchedException() {
+		return catchedException;
+	}
+
+	public void setCatchedException(boolean catchedException) {
+		this.catchedException = catchedException;
+	}
+
+	public boolean isCommandFound() {
+		return commandFound;
+	}
+
+	public void setCommandFound(boolean commandFound) {
+		this.commandFound = commandFound;
 	}
   
+	
+	
+	
 }
--- Assignment1/src/ist/meic/pa/test/TestClassThrowsException.java
+++ Assignment1/src/ist/meic/pa/test/TestClassThrowsException.java
@@ -1,6 +1,6 @@
 package ist.meic.pa.test;
 
-public class TestClass {
+public class TestClassThrowsException {
 	public static void main(String[] args){
 		throw new ExceptionFoundException();
 	}
